8c67709580d32d520f3522611e3252cbab548930
Fernando Garcia Alvarez <fercarcedo@gmail.com>
2017-04-04 14:34:01 +0200

    Mejorado script de arranque

--- startkafka.bat
+++ startkafka.bat
@@ -1,4 +1,4 @@
 cd bin/windows
 start "Zookeeper" cmd /c zookeeper-server-start.bat ../../config/zookeeper.properties
-timeout /t 5 /nobreak > NUL
+timeout /t 10 /nobreak > NUL
 start "Apache Kafka" cmd /c kafka-server-start.bat ../../config/server.properties
