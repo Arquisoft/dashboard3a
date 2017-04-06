527f86d01e92328d7a6a3639199e22ec139ccd65
Andres Gargano <uo247242@uniovi.es>
2017-04-06 05:52:57 +0200

    - Añadidos sleeps para ver si Selenium reacciona a tiempo

--- src/test/java/asw/dashboard/cucumber/DashboardLoginStep.java
+++ src/test/java/asw/dashboard/cucumber/DashboardLoginStep.java
@@ -15,27 +15,51 @@ public class DashboardLoginStep {
 	private WebDriver driver = (WebDriver) new FirefoxDriver();
 
 	@Given("^the politician is in the login page$")
-	public void isInLoginPage() {		
+	public void isInLoginPage() {	
+		sleepOneSecond();
 		driver.navigate().to("http://localhost:8090/");
-		
+
+		sleepOneSecond();		
 		driver.findElement(By.id("email"));
+		
+		sleepOneSecond();
 		driver.findElement(By.id("password"));
 	}
 
 	@Then("^introduces his credentials, \"(.+)\" and \"(.+)\" into the login form$")
 	public void insertCredentials(String mail, String psw) {
+		
+		sleepOneSecond();
 		driver.findElement(By.id("email")).sendKeys(mail);
+		
+		sleepOneSecond();
 		driver.findElement(By.id("password")).sendKeys(psw);
 	}
 
 	@When("^he pushes the \"Log in\" button$")
 	public void pushLogIn() {	
+		
+		sleepOneSecond();
 		 driver.findElement(By.id("loginButton")).click();
 	}
 
 	@And("^he gets redirected to the dashboard view$")
 	public void isInDashboard() {	
-		driver.findElement(By.id("tablaSugerencias"));		
+		
+		sleepOneSecond();
+		driver.findElement(By.id("tablaSugerencias"));	
+		
+		sleepOneSecond();
 		driver.quit();
+	}
+	
+	private void sleepOneSecond() {
+		
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
